a3af0370c707ea720dac225e4796caf587b66679
AOmwenga <aomwenga@gmail.com>
2024-10-11 10:08:18 -0500

    search and drop down

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,5 +1,5 @@
 // event listener to the button that calls the fetchCatImage function when clicked
-document.getElementById("new-dog").addEventListener("click", fetchCatImage);
+document.getElementById("new-cat").addEventListener("click", fetchCatImage);
 
 // async function to fetch a random cat image from The Cat API
 async function fetchCatImage() {
@@ -35,3 +35,67 @@ function displayCatImage(imageUrl) {
 
 // initial cat image when the page loads
 fetchCatImage();
+
+// Create user interaction with the API through a search feature, paginated gallery, or similar. This feature should use GET requests to retrieve associated data.
+// event listener
+document
+  .getElementById("search-cat")
+  .addEventListener("click", searchCatByBreed);
+
+// fetch initial cat image and populate the breed list on page load
+fetchCatImage();
+fetchBreeds();
+// get list of cat breeds from The Cat API
+async function fetchBreeds() {
+  try {
+    const response = await fetch("https://api.thecatapi.com/v1/breeds", {
+      headers: {
+        "x-api-key":
+          "live_7JHK2hfszqskVylVAVpPIPRyPADDs6zzCDz1mNvlaD1H6wHVhnBVT4rNKXOiojoD", // Include the API key in the request headers
+      },
+    });
+    // json response to get the breed data
+    const breeds = await response.json();
+    // populate the breed dropdown list
+    populateBreedList(breeds);
+  } catch (error) {
+    // errors that occur during the fetch request
+    console.error("Error fetching the breeds:", error);
+  }
+}
+
+// show the breed dropdown list
+function populateBreedList(breeds) {
+  const breedSelect = document.getElementById("breed-select");
+  breeds.forEach((breed) => {
+    const option = document.createElement("option");
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
+}
+
+// search for cat images by breed
+async function searchCatByBreed() {
+  const breedSelect = document.getElementById("breed-select");
+  const breedId = breedSelect.value;
+
+  try {
+    const response = await fetch(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+      {
+        headers: {
+          "x-api-key":
+            "live_7JHK2hfszqskVylVAVpPIPRyPADDs6zzCDz1mNvlaD1H6wHVhnBVT4rNKXOiojoD", // Include the API key in the request headers
+        },
+      }
+    );
+    // json response to get the image data
+    const data = await response.json();
+    // show the image in the gallery
+    displayCatImage(data[0].url);
+  } catch (error) {
+    // errors that occur during the fetch request
+    console.error("Error fetching the cat images by breed:", error);
+  }
+}
